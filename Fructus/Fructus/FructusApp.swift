5d9889862695bf1f196ec8299ad9d31887f7d802
Adam Zarn <ajzarn@gmail.com>
2021-02-09 11:04:56 -0600

    Add Fructus app

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Adam Zarn on 2/9/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
